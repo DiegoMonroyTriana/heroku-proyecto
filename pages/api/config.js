bd6d7719bc001a889b0e1e4b8779fa27be064e80
DiegoMonroyTriana <46665705+DiegoMonroyTriana@users.noreply.github.com>
2022-03-28 14:12:13 -0600

    Update config.js

--- pages/api/config.js
+++ pages/api/config.js
@@ -1,4 +1,4 @@
 export default {
   SECRET: 'asudyftae712!@#$%#@4aowuiecdhaiduc84726',
-  URL: 'https://heroku-proyecto.vercel.app/api/'
+  URL: 'https://heroku-proyecto-9d72a4idi-diegomonroytriana.vercel.app/api/'
 }
