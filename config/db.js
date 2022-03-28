48a57ed9ce47a99b8c6d5eee44e74fe0e0cc3be7
DiegoMonroyTriana <46665705+DiegoMonroyTriana@users.noreply.github.com>
2022-03-28 16:25:59 -0600

    Update db.js

--- config/db.js
+++ config/db.js
@@ -7,11 +7,19 @@ import { createPool } from 'mysql2/promise'
   //   port: 3306,
   //   database: 'proyectos'
   // })
- const pool = createPool({
-   host: 'us-cdbr-east-05.cleardb.net',
-   user: 'bf90f8e2ab99c2',
-   password: 'ae490b85',
-   database: 'heroku_fbe71ac25acd3dc'
+ //const pool = createPool({
+ //  host: 'us-cdbr-east-05.cleardb.net',
+ //  user: 'bf90f8e2ab99c2',
+ //  password: 'ae490b85',
+ //  database: 'heroku_fbe71ac25acd3dc'
+ //})
+ 
+ const pool = createPool ({
+  hsot: 'db5006930211.hosting-data.io',
+   user:  'dbu1582628',
+   password: 'qKBUmFbi%!se', 
+   port: 3306,
+   database: 'dbs5721780' 
  })
 
 export { pool }
