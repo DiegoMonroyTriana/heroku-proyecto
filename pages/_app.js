c647847b9fbeb198720d1d5f877b4004bb74310e
DiegoMonroyTriana <46665705+DiegoMonroyTriana@users.noreply.github.com>
2022-03-28 12:41:37 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,11 @@ import Head  from 'next/head'
 function MyApp ({ Component, pageProps }) {
   return (
    <ChakraProvider>
+     <Head>
+      <meta name = 'description' content = 'Autor: Diego Monroy Triana, Categoria: Administración' />
+      <title>Proyectos</title>
+      <link rel="shortcut icon" href="/logo.png" />
+    </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   )
