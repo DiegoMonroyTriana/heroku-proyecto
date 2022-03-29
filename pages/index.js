784e04804f9f96e67e7266afc3241616afd9f8ee
DiegoMonroyTriana <46665705+DiegoMonroyTriana@users.noreply.github.com>
2022-03-29 11:55:23 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import { SigninForm } from '../components/usuarios/SigninForm'
 export default function HomePage () {
   return (
-    <div className='container min-h-screen grid place-items-center'>
-      <SigninForm/>
+    <div className='flex justify-center items-center content-center min-h-screen w-full'>
+      <div className='w-full max-w-md'>
+      <SigninForm />
+      </div>
     </div>
   )
 }
