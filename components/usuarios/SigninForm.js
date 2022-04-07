63ea034f8b656e665267bd4caf0c094a9edd7520
DiegoMonroyTriana <46665705+DiegoMonroyTriana@users.noreply.github.com>
2022-04-07 15:05:49 -0500

    Cambio de alerta

--- components/usuarios/SigninForm.js
+++ components/usuarios/SigninForm.js
@@ -1,15 +1,13 @@
 import { Input, Spacer, Text } from '@nextui-org/react'
-import { Button, AlertDialog, AlertDialogOverlay, AlertDialogContent, AlertDialogBody, AlertDialogFooter } from '@chakra-ui/react'
+import { Button } from '@chakra-ui/react'
 import { useForm } from 'react-hook-form'
 import axios from 'axios'
 import React, { useEffect } from 'react'
 import Cookie from 'universal-cookie'
 import config from '../../pages/api/config'
+import Swal from 'sweetalert2'
 
 export function SigninForm () {
-  const [isOpen, setIsOpen] = React.useState(false)
-  const onClose = () => setIsOpen(false)
-  const cancelRef = React.useRef()
   const { register, handleSubmit } = useForm()
   const onSubmit = async (data) => {
     try {
@@ -18,7 +16,12 @@ export function SigninForm () {
       cookie.set('session', result.data, { path: '/' })
       window.location.href = '/inicio'
     } catch (error) {
-      setIsOpen(true)
+      Swal.fire({
+        title: 'Usuario o contraseña incorrectos',
+        text: 'vovler a intentarlo',
+        icon: 'error',
+        confirmButtonText: 'Aceptar'
+      })
     }
   }
 
@@ -37,29 +40,13 @@ export function SigninForm () {
             <Spacer y={1.5} />
             <Input width="80%" type = 'email' clearable underlined labelPlaceholder="Email" {...register('email', { required: true })} />
             <Spacer y={1.5} />
-            <Input.Password width="80%" clearable underlined labelPlaceholder="Contraseña" {...register('password', { required: true })} />
+            <Input.Password width="80%" clearable underlined labelPlaceholder="Contraseña" required {...register('password', { required: true })} />
             <Spacer y={1.5} />
             <div className = "flex place-content-center">
                  <Button type='submit' variant= "solid" colorScheme="whatsapp">Iniciar Sesión</Button>
             </div>
         </form>
-
-        <AlertDialog
-        isOpen={isOpen}
-        leastDestructiveRef={cancelRef}
-        onClose={onClose}>
-        <AlertDialogOverlay size="sm">
-          <AlertDialogContent>
-          <Spacer y={1} />
-            <AlertDialogBody className='text-2xl text-center'>Datos incorrectos</AlertDialogBody>
-            <AlertDialogFooter>
-              <Button size="sm" ref={cancelRef} onClick={onClose}>
-                Ok
-              </Button>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialogOverlay>
-      </AlertDialog>
       </>
+      
   )
 }
